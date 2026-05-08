08ef2a563f03cf311281a9e404aea5f051aa9004
oregondude77 <geoff.tracy@alert360.com>
2026-05-08 14:21:24 -0500

    fixed phone scaling and moved night/day toggle to floating button

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,3 +14,12 @@ const eslintConfig = defineConfig([
 ]);
 
 export default eslintConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: {
+    position: "bottom-right",
+  },
+};
+
+export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  devIndicators: {
+    position: "bottom-right",
+  },
 };
 
 export default nextConfig;
